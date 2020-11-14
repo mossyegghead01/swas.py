79753679814700768f576964d27f99c15e6c00df
mossyegghead01 <69193776+mossyegghead01@users.noreply.github.com>
2020-11-14 17:20:34 +0700

    Update info.py

--- swas/info.py
+++ swas/info.py
@@ -1,2 +1,7 @@
 def yt():
   return "https://www.youtube.com/channel/UC2ITRZ4_Di-KMHSIylTQbBA"
+
+class userinfo():
+  def __init__(self):
+    self.name = "Swas.py"
+    self.id = 556119013298667520
