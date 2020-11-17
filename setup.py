68170b4b45335a58245626e851c405a4e3322903
mossyegghead01 <69193776+mossyegghead01@users.noreply.github.com>
2020-11-17 14:59:23 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='swas.py',
     packages=find_packages(include=['swas']),
-    version='0.0.1',
+    version='0.0.3',
     description='a joke library made for a discord user',
     author='mossy the made guy',
     license='MIT',
