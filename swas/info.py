a0c702db2e7bea7aa86812b12c1b8800a2653133
Swas.py <61446939+CodeWithSwastik@users.noreply.github.com>
2020-12-27 11:06:11 +0530

    Added wiki

--- swas/info.py
+++ swas/info.py
@@ -6,3 +6,6 @@ class userinfo():
     self.name = "Swas.py"
     self.discriminator = 7370
     self.id = 556119013298667520    
+
+def wiki():
+  return "https://youtube.fandom.com/wiki/Code_With_Swastik"
