0659f30c58601a021082704b487e2d8b1cb2f38e
mossyegghead01 <69193776+mossyegghead01@users.noreply.github.com>
2020-12-28 10:16:16 +0700

    Version bump to 0.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setup(
     name='swas.py',
     packages=find_packages(include=['swas']),
-    version='0.0.4',
+    version='0.0.5',
     description='a joke library made for a discord user',
     long_description=long_description,
     long_description_content_type="text/markdown",
